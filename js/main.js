a386068684b71b33d680ff87d79890728af9678c
kushal25A <kushalphaiju@gmail.com>
2020-01-24 07:21:30 +0545

    project start

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function LoginForm(){
+	document.getElementById("loginform").style.display ="block";
+}
+	function closeForm(){
+		document.getElementById("myForm").style.display="none";
+}
+function (){
+ document.getElementById("login").style.display ="block";
+}
+
+alert("This us 00");
